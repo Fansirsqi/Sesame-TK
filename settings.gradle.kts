5fed46512a1fe68aba83320ccfa8f4e3349a12e5
1314520lovelsc <69620116+1314520lovelsc@users.noreply.github.com>
2025-08-08 08:57:43 +0800

    Update settings.gradle.kts
    更新阿里云源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         mavenCentral()
         gradlePluginPortal()
         google()
@@ -11,7 +14,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/spring")
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         mavenCentral()
         google()
     }
